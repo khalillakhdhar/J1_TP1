c846cd94ca97ad8e96398a9617675876b3d9b652
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-20 11:30:22 +0100

    first

---
+++ src/calcules/Math.java
@@ -0,0 +1,5 @@
+package calcules;
+
+public class Math {
+
+}
---
+++ src/servlet/Reading.java
@@ -0,0 +1,51 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Reading
+ */
+@WebServlet("/Reading")
+public class Reading extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Reading() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String nom=request.getParameter("nom");
+		PrintWriter out=response.getWriter();
+		out.println("bienvenu"+nom);
+		request.setAttribute("nom",nom);
+		request.getRequestDispatcher("resultat.jsp").forward(request, response);;
+	
+		
+	}
+
+}
